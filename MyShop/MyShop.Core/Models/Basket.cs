76dbf9a98cc92a2b651794373d6a815b1c6ff0bd
czelijm <czelijm@outlook.com>
2020-06-20 15:56:57 +0200

    Added the Basket Model

---
+++ MyShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Basket : BaseEntity
+    {
+        //virtual to use lazy loading, while loading the basket, entityframework will load all basketItems
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+        }
+    }
+}
